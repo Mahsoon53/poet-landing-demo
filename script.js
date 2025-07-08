42b8528bc3b6b1523e8bba1fb8938e1b5f8f3db4
Mahsoon53 <clickhot7@gmail.com>
2025-07-08 23:45:22 +0330

    Update script.js
    ببینیم چی میشه

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // تو این مرحله فقط برای توسعه آماده‌ست
 // می‌تونی تو مراحل بعدی منوی همبرگری یا انیمیشن‌ها رو اینجا اضافه کنی
 console.log("صفحه اصلی 'صدای شاعر' بارگذاری شد.");
+function toggleMenu() {
+  const menu = document.getElementById("mainMenu");
+  menu.classList.toggle("active");
+}
